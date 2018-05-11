14784cad35f30eee5bca5c2745c54501e7691382
Andrew Sergeev <asergeev@glosav.ru>
2018-05-11 15:46:26 +0300

    tmp

--- src/main/java/ru/glosav/gais/gateway/dto/Application.java
+++ src/main/java/ru/glosav/gais/gateway/dto/Application.java
@@ -29,7 +29,7 @@ public class Application {
     private String sessionId;
     @NotNull
     @Column(unique = true)
-    @ApiModelProperty(value = "Номер заявки")
+    @ApiModelProperty(value = "Номер заявки", example = "25042018")
     private String number;
     @NotNull
     @ApiModelProperty(value = "Дата заявки, формат dd.MM.yyyy", example = "25.04.2018")
@@ -37,107 +37,15 @@ public class Application {
     @JsonFormat(pattern="dd.MM.yyyy", shape = JsonFormat.Shape.STRING)
     private Date appDate;
     @NotNull
-    @ApiModelProperty(value = "Документ - основание заявки")
+    @ApiModelProperty(value = "Документ - основание заявки", example = "Договор")
     private String base; // основание
     @NotNull
-    @ApiModelProperty(value = "Номер документа - основания заявки")
+    @ApiModelProperty(value = "Номер документа - основания заявки", example = "2504")
     private String baseNumber; //
     @ApiModelProperty(value = "Заявляемая компания")
     @NotNull
     @ManyToOne(cascade = {CascadeType.PERSIST})
     private Company company;
 
-    public static Group toTrift(
-            DispatchBackend.Client client,
-            dispatch.server.thrift.backend.Session s,
-            String groupName,
-            Application a) throws Exception {
-
-        List<Group> groups = client.getRootGroups(s);
-
-        final Group[] russianCarrierGroup = new Group[1];
-
-        groups.stream().forEach(group -> {
-            if (group.getTitle().equals(groupName))
-                russianCarrierGroup[0] = group;
-        });
-
-/*
-
-
-        if (russianCarrierGroup == null) {
-
-            List<Group> childGroups = client.getChildrenGroups(
-                    s,
-                    primaryGroup.getId(),
-                    true
-            );
-
-            for (Group g : childGroups) {
-                if (g.title.equals(m_groupname)) {
-                    russianCarrierGroup = g;
-                    break;
-                }
-            }
-        }
-*/
-        /////////////////////// Создаем компанию //////////////////////
-
-        List<StoreFieldValue> extraFields = new ArrayList<StoreFieldValue>();
-        // обращаю внимание на то, что значения Title должны добуквенно
-        // соответствовать указанным, а в значения Value подставляться
-        // актуальные данные. Наименование организации в extraFields
-        // НЕ указывается
-        extraFields.add(new StoreFieldValue()
-                .setTitle("№ Договора")
-                .setValue(a.getBaseNumber())); // "44567789900"
-        extraFields.add(new StoreFieldValue()
-                .setTitle("Дата Договора")
-                .setValue(
-                        DateUtil.parse(a.getAppDate()) // "01-01-2018"
-                ));
-        extraFields.add(new StoreFieldValue()
-                .setTitle("ИНН")
-                .setValue(a.getCompany().getInn())); // "155115802"
-        extraFields.add(new StoreFieldValue()
-                .setTitle("КПП")
-                .setValue(a.getCompany().getKpp())); // "123423"
-        extraFields.add(new StoreFieldValue()
-                .setTitle("Адрес организации")
-                .setValue(a.getCompany().getPaddress())); // "Ленинградское шоссе 80 к 16"
-        extraFields.add(new StoreFieldValue()
-                .setTitle("E-mail")
-                .setValue(a.getCompany().getEmail())); // "eprosso@navitel.su"
-        extraFields.add(new StoreFieldValue()
-                .setTitle("Телефон")
-                .setValue(a.getCompany().getPhone())); // "22233222"
-        extraFields.add(new StoreFieldValue()
-                .setTitle("Идентификатор ЕГИС ОТБ")
-                .setValue(a.getCompany().getEgisOtbId())); // "ОТБ-1-2"
-
-        License license = new License();
-        // Время истечения лицензии в формате Unix timestamp,
-        // 1546214400 соответствует 31.12.2018.
-        // Можно не указывать для бесконечных лицензий
-        if (a.getCompany().getExpireLicense() != null)
-            license.setExpire(a.getCompany().getExpireLicense().getTime() / 1000);
-        // Максимальное количество ОМ у перевозчика.
-        // Можно не указывать, если не лимитировано особо
-        // license.setMonitoringObjectsLimit(100);
-        // Максимальное количество пользователей.
-        // Для пользователей СИР число всегда равно 1
-        license.setUsersLimit(1);
-        // создаваемая компания находится во включенном состоянии
-        license.setEnabled(true);
-
-        // создается компания-перевозчик с подготовленными параметрами:
-        Group group
-                = client.createCompanyWithAdditionalFields(s, russianCarrierGroup[0].getId(),
-                a.getCompany().getName(), // "ООО Адам Козлевич"
-                license,
-                new AdditionalFields(extraFields)
-        );
-        return group;
-    }
 
 }
--- src/main/java/ru/glosav/gais/gateway/svc/GaisConnectorService.java
+++ src/main/java/ru/glosav/gais/gateway/svc/GaisConnectorService.java
@@ -1,8 +1,6 @@
 package ru.glosav.gais.gateway.svc;
 
-import dispatch.server.thrift.backend.DispatchBackend;
-import dispatch.server.thrift.backend.Group;
-import dispatch.server.thrift.backend.Session;
+import dispatch.server.thrift.backend.*;
 import org.apache.thrift.TException;
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.transport.TFramedTransport;
@@ -15,9 +13,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ru.glosav.gais.gateway.cfg.GaisClientConfig;
 import ru.glosav.gais.gateway.dto.Application;
+import ru.glosav.gais.gateway.util.DateUtil;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -96,5 +96,100 @@ public class GaisConnectorService {
         }
 
     }
+
+    public Group save(
+            String groupName,
+            Application a) throws Exception {
+
+        List<Group> groups = client.getRootGroups(session);
+
+        final Group[] russianCarrierGroup = new Group[1];
+
+        groups.stream().forEach(group -> {
+            if (group.getTitle().equals(groupName))
+                russianCarrierGroup[0] = group;
+        });
+
+/*
+
+
+        if (russianCarrierGroup == null) {
+
+            List<Group> childGroups = client.getChildrenGroups(
+                    s,
+                    primaryGroup.getId(),
+                    true
+            );
+
+            for (Group g : childGroups) {
+                if (g.title.equals(m_groupname)) {
+                    russianCarrierGroup = g;
+                    break;
+                }
+            }
+        }
+*/
+        /////////////////////// Создаем компанию //////////////////////
+
+        List<StoreFieldValue> extraFields = new ArrayList<StoreFieldValue>();
+        // обращаю внимание на то, что значения Title должны добуквенно
+        // соответствовать указанным, а в значения Value подставляться
+        // актуальные данные. Наименование организации в extraFields
+        // НЕ указывается
+        extraFields.add(new StoreFieldValue()
+                .setTitle("№ Договора")
+                .setValue(a.getBaseNumber())); // "44567789900"
+        extraFields.add(new StoreFieldValue()
+                .setTitle("Дата Договора")
+                .setValue(
+                        DateUtil.parse(a.getAppDate()) // "01-01-2018"
+                ));
+        extraFields.add(new StoreFieldValue()
+                .setTitle("ИНН")
+                .setValue(a.getCompany().getInn())); // "155115802"
+        extraFields.add(new StoreFieldValue()
+                .setTitle("КПП")
+                .setValue(a.getCompany().getKpp())); // "123423"
+        extraFields.add(new StoreFieldValue()
+                .setTitle("Адрес организации")
+                .setValue(a.getCompany().getPaddress())); // "Ленинградское шоссе 80 к 16"
+        extraFields.add(new StoreFieldValue()
+                .setTitle("E-mail")
+                .setValue(a.getCompany().getEmail())); // "eprosso@navitel.su"
+        extraFields.add(new StoreFieldValue()
+                .setTitle("Телефон")
+                .setValue(a.getCompany().getPhone())); // "22233222"
+        extraFields.add(new StoreFieldValue()
+                .setTitle("Идентификатор ЕГИС ОТБ")
+                .setValue(a.getCompany().getEgisOtbId())); // "ОТБ-1-2"
+        extraFields.add(new StoreFieldValue()
+                .setTitle("extSessionId")
+                .setValue(a.getSessionId())); //
+
+        License license = new License();
+        // Время истечения лицензии в формате Unix timestamp,
+        // 1546214400 соответствует 31.12.2018.
+        // Можно не указывать для бесконечных лицензий
+        if (a.getCompany().getExpireLicense() != null)
+            license.setExpire(a.getCompany().getExpireLicense().getTime() / 1000);
+        // Максимальное количество ОМ у перевозчика.
+        // Можно не указывать, если не лимитировано особо
+        // license.setMonitoringObjectsLimit(100);
+        // Максимальное количество пользователей.
+        // Для пользователей СИР число всегда равно 1
+        license.setUsersLimit(1);
+        // создаваемая компания находится во включенном состоянии
+        license.setEnabled(true);
+
+        // создается компания-перевозчик с подготовленными параметрами:
+        Group group
+                = client.createCompanyWithAdditionalFields(session, russianCarrierGroup[0].getId(),
+                a.getCompany().getName(), // "ООО Адам Козлевич"
+                license,
+                new AdditionalFields(extraFields)
+        );
+        return group;
+    }
+
 
 }
